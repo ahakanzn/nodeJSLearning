e078acb4462f92cc5375ed7729638baa603dbc31
A.Hakan <a.hakan.zn@gmail.com>
2022-03-13 20:52:26 +0300

    db insert functions

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,5 @@
-import { Console } from "console";
-import express, { Request, Response, Express, NextFunction, response } from "express"
+import express, { Request, response, Response } from "express"
 
-
-import { TypeFormatFlags } from "typescript";
 //Make a simple calculator
 /*
 1- create 4 endpoint
@@ -21,11 +18,70 @@ All endpoints will return result in the format given below;
   status: boolean  
 }
 */
-const dbURL = 'mongodb+srv://ahakanzn:***.@cluster0.gc27e.mongodb.net/**?retryWrites=true&w=majority'
-const mongoose = require('mongoose');
-mongoose.connect(dbURL, {useNewUrlParse: true, useUnifiedTopology: true})
-  .then((result: Function)=> console.log('Connection success'))
-  .catch((err: Function)=> console.log('Error!!'))
+const dbURL = process.env.DATABASE_URI!
+
+import { MongoClient } from "mongodb";
+const client = new MongoClient("mongodb+srv://ahakanzn:Ahmet123.@cluster0.gc27e.mongodb.net/History?retryWrites=true&w=majority");
+const db = client.db('History');
+
+const AdditionCollection = db.collection("AdditionCollections")
+async function insertAdditionToDB(req: Request, total: number) {
+  try {
+    const result = await AdditionCollection.insertOne({
+      apple: req.body.apple,
+      pear: req.body.pear,
+      total: total
+    });
+    console.log(`A document was inserted with the _id: ${result.insertedId}`);
+  } catch (err: any) {
+    console.log(err.stack);
+  }
+}
+
+const substractionCollection = db.collection("substractionCollection")
+async function insertSubstractionToDB(req: Request, total: number) {
+  try {
+    const result = await substractionCollection.insertOne({
+      apple: req.body.apple,
+      pear: req.body.pear,
+      total: total
+    });
+    console.log(`A document was inserted with the _id: ${result.insertedId}`);
+  } catch (err: any) {
+    console.log(err.stack);
+  }
+}
+
+const multiplicationCollection = db.collection("AdditionCollections")
+async function insertMulToDB(req: Request, total: number) {
+  try {
+    const result = await multiplicationCollection.insertOne({
+      apple: req.body.apple,
+      pear: req.body.pear,
+      total: total
+    });
+    console.log(`A document was inserted with the _id: ${result.insertedId}`);
+  } catch (err: any) {
+    console.log(err.stack);
+  }
+}
+
+const divisionCollection = db.collection("AdditionCollections")
+async function insertDivisionToDB(req: Request, total: number) {
+  try {
+    const result = await divisionCollection.insertOne({
+      apple: req.body.apple,
+      pear: req.body.pear,
+      total: total
+    });
+    console.log(`A document was inserted with the _id: ${result.insertedId}`);
+  } catch (err: any) {
+    console.log(err.stack);
+  }
+}
+
+
+
 
 const app = express()
 app.use(express.json());
@@ -39,6 +95,7 @@ var calculator = require('./calculator');
 const add = (req: Request<any, any, { apple: number, pear: number }>, res: Response) => {
   var calResult = calculator.addFunction(req.body.apple, req.body.pear);
   if (!isNaN(calResult)) {
+    insertAdditionToDB(req, calResult).catch(console.dir);
     res.json({ result: calResult, status: "OK" });
   }
   else {
@@ -49,6 +106,7 @@ const add = (req: Request<any, any, { apple: number, pear: number }>, res: Respo
 const sub = (req: Request<any, any, { apple: number, pear: number }>, res: Response) => {
   var calResult = calculator.subFunction(req.body.apple, req.body.pear);
   if (!isNaN(calResult)) {
+    insertSubstractionToDB(req, calResult).catch(console.dir);
     res.json({ result: calResult, status: "OK" });
   }
   else {
@@ -59,6 +117,7 @@ const sub = (req: Request<any, any, { apple: number, pear: number }>, res: Respo
 const multiply = (req: Request<any, any, { apple: number, pear: number }>, res: Response) => {
   var calResult = calculator.multiplyFunction(req.body.apple, req.body.pear);
   if (!isNaN(calResult)) {
+    insertMulToDB(req, calResult).catch(console.dir);
     res.json({ result: calResult, status: "OK" });
   }
   else {
@@ -69,6 +128,7 @@ const multiply = (req: Request<any, any, { apple: number, pear: number }>, res: 
 const divide = (req: Request<any, any, { apple: number, pear: number }>, res: Response) => {
   var calResult = calculator.divideFunction(req.body.apple, req.body.pear);
   if (!isNaN(calResult)) {
+    insertDivisionToDB(req, calResult).catch(console.dir);
     res.json({ result: calResult, status: "OK" });
   }
   else {
@@ -76,6 +136,15 @@ const divide = (req: Request<any, any, { apple: number, pear: number }>, res: Re
   }
 };
 
+const getCalculatorHistory = async (res: Response) => {
+  const additions = AdditionCollection.find({}).toArray();
+  try {
+    res.json({res: "zort"});
+  } catch (error) {
+    //res.json({res: "hata"})
+  }
+  
+};
 
 //Routes
 app.post('/add', add)
@@ -86,6 +155,10 @@ app.post('/mul', multiply)
 
 app.post('/div', divide)
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+app.get('/getAddtitionHistory', getCalculatorHistory)
+
+client.connect().then(() => {
+  app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+  })
 })
